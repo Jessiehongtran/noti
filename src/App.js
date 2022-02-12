be70c0bc82c6d8534f4dc0e39394baaa2e50747c
Hong Tran <jessietran@fb.com>
2022-02-11 23:44:24 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.showNotification = this.showNotification.bind(this);
+  }
+
+  componentDidMount(){
+    if (!("Notification" in window)){
+      console.log("This browser does not support desktop notification")
+    } else {
+      // console.log("Notifications are supported")
+      Notification.requestPermission()
+    }
+  }
+
+  showNotification(){
+    new Notification('Hey')
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.showNotification}>
+          Click to show notification
+        </button>
+      </div>
+    )
+  }
 }
 
-export default App;
